d2ffeb2f8a654774d16e062f0696ae738e0018d0
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-08-12 17:13:50 -0400

    Create Query-DCOM.ps1
    Still needs a couple of adjustments. I worked on everything myself with the exception of the Default SID's, that default SID table is from picheljitsu.
    
    Will add other features that query AD as well and gets those SIDs checked too.

---
+++ Query-DCOM.ps1
@@ -0,0 +1,196 @@
+#_____________________________________________________________________________________________________________________________________________________
+#//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\--//--\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//  [ Secure Digits Plus LLC | Hybrid | Desired State Controller ]  \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯     __________________________________________________________     ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯ -=-=-=-=-=-=-=[ Dynamically Engineered Digital Security ]-=-=-=-=-=-=--=-= ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯                                           ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//   Application Development | Virtualization | Network and Hardware Magistration   \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//   https://www.securedigitsplus.com | Server-Client | Seedling-Spawning Script    \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\___¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ___//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   [ Provisional Author : Michael C Cook Sr. | "The Buck Stops Here" ]    //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//   ____    ____    ____    ____    ____    ____    ____    ____    ____   \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+#//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\
+#\\  [ Install-Hybrid ] @: Installs the base folder and application prerequisites for initializing a Hybrid Desired State Controller Server  //  \\__//
+#//__________________________________________________________________________________________________________________________________________\\__//¯¯\\
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//
+                                                                                                                                     #\__//¯¯\\__//¯¯\\
+
+# https://github.com/picheljitsu/Powershell/blob/master/ThreatHunting/Get-DCOMSecurity.ps1 | This list is based off of Matt Pichelmayer and this script
+
+<#
+
+ .SYNOPSIS
+ Checks DCOM Applications, Access, and Launch Settings to display on the screen
+
+ .DESCRIPTION
+ For identifying any DCOM settings that may be unknown or not part of your system
+
+ .USAGE
+ Not quite done, but when finished, will display any singlestring or / piped arrays of GUIDs
+ with associated Access/Launch permissions to screen
+
+#>
+
+    Function Collect-DefaultSID 
+    {
+        Return [ Ordered ]@{ 
+    	    "S-1-0"                      = "Null Authority"
+    	    "S-1-0-0"                    = "Nobody"
+	        "S-1-1"                      = "World Authority"
+	        "S-1-1-0"                    = "Everyone"
+    	    "S-1-2"                      = "Local Authority"
+	        "S-1-2-0"                    = "Local"
+	        "S-1-2-1"                    = "Console Logon"
+    	    "S-1-3"                      = "Creator Authority"
+	        "S-1-3-0"                    = "Creator Owner"
+	        "S-1-3-1"                    = "Creator Group"
+    	    "S-1-3-2"                    = "Creator Owner Server"
+	        "S-1-3-3"                    = "Creator Group Server"
+	        "S-1-3-4 Name: Owner Rights" = "SID: S-1-3-4 Owner Rights"
+    	    "S-1-4"                      = "Non-unique Authority"
+	        "S-1-5"                      = "NT Authority"
+	        "S-1-5-1"                    = "Dialup"
+    	    "S-1-5-2"                    = "Network"
+	        "S-1-5-3"                    = "Batch"
+	        "S-1-5-4"                    = "Interactive"
+    	    "S-1-5-5-X-Y"                = "Logon Session"
+	        "S-1-5-6"                    = "Service"
+	        "S-1-5-7"                    = "Anonymous"
+    	    "S-1-5-8"                    = "Proxy"
+	        "S-1-5-9"                    = "Enterprise Domain Controllers"
+	        "S-1-5-10"                   = "Principal Self"
+    	    "S-1-5-11"                   = "Authenticated Users"
+	        "S-1-5-12"                   = "Restricted Code"
+	        "S-1-5-13"                   = "Terminal Server Users"
+    	    "S-1-5-14"                   = "Remote Interactive Logon"
+	        "S-1-5-15"                   = "This Organization"
+	        "S-1-5-17"                   = "This Organization"
+    	    "S-1-5-18"                   = "Local System"
+	        "S-1-5-19"                   = "NT Authority"
+	        "S-1-5-20"                   = "NT Authority"
+    	    "S-1-5-21domain-500"         = "Administrator"
+	        "S-1-5-21domain-501"         = "Guest"
+	        "S-1-5-21domain-502"         = "KRBTGT"
+    	    "S-1-5-21domain-512"         = "Domain Admins"
+	        "S-1-5-21domain-513"         = "Domain Users"
+	        "S-1-5-21domain-514"         = "Domain Guests"
+    	    "S-1-5-21domain-515"         = "Domain Computers"
+	        "S-1-5-21domain-516"         = "Domain Controllers"
+	        "S-1-5-21domain-517"         = "Cert Publishers"
+    	    "S-1-5-21root domain-518"    = "Schema Admins"
+	        "S-1-5-21root domain-519"    = "Enterprise Admins"
+	        "S-1-5-21domain-520"         = "Group Policy Creator Owners"
+    	    "S-1-5-21domain-526"         = "Key Admins"
+	        "S-1-5-21domain-527"         = "Enterprise Key Admins"
+	        "S-1-5-21domain-553"         = "RAS and IAS Servers"
+    	    "S-1-5-32-544"               = "Administrators"
+	        "S-1-5-32-545"               = "Users"
+	        "S-1-5-32-546"               = "Guests"
+    	    "S-1-5-32-547"               = "Power Users"
+	        "S-1-5-32-548"               = "Account Operators"
+	        "S-1-5-32-549"               = "Server Operators"
+    	    "S-1-5-32-550"               = "Print Operators"
+	        "S-1-5-32-551"               = "Backup Operators"
+	        "S-1-5-32-552"               = "Replicators"
+    	    "S-1-5-64-10"                = "NTLM Authentication"
+	        "S-1-5-64-14"                = "SChannel Authentication"
+	        "S-1-5-64-21"                = "Digest Authentication"
+    	    "S-1-5-80"                   = "NT Service"
+	        "S-1-5-80-0"                 = "All Services"
+	        "S-1-5-83-0"                 = "NT VIRTUAL MACHINE\Virtual Machines"
+    	    "S-1-16-0"                   = "Untrusted Mandatory Level"
+	        "S-1-16-4096"                = "Low Mandatory Level"
+	        "S-1-16-8192"                = "Medium Mandatory Level"
+    	    "S-1-16-8448"                = "Medium Plus Mandatory Level"
+	        "S-1-16-12288"               = "High Mandatory Level"
+	        "S-1-16-16384"               = "System Mandatory Level"
+    	    "S-1-16-20480"               = "Protected Process Mandatory Level"
+	        "S-1-16-28672"               = "Secure Process Mandatory Level"
+	        "S-1-5-32-554"               = "BUILTIN\Pre-Windows 2000 Compatible Access"
+    	    "S-1-5-32-555"               = "BUILTIN\Remote Desktop Users"
+	        "S-1-5-32-556"               = "BUILTIN\Network Configuration Operators"
+	        "S-1-5-32-557"               = "BUILTIN\Incoming Forest Trust Builders"
+    	    "S-1-5-32-558"               = "BUILTIN\Performance Monitor Users"
+	        "S-1-5-32-559"               = "BUILTIN\Performance Log Users"
+	        "S-1-5-32-560"               = "BUILTIN\Windows Authorization Access Group"
+    	    "S-1-5-32-561"               = "BUILTIN\Terminal Server License Servers"
+	        "S-1-5-32-562"               = "BUILTIN\Distributed COM Users"
+	        "S-1-5- 21domain -498"       = "Enterprise Read-only Domain Controllers"
+    	    "S-1-5- 21domain -521"       = "Read-only Domain Controllers"
+	        "S-1-5-32-569"               = "BUILTIN\Cryptographic Operators"
+	        "S-1-5-21 domain -571"       = "Allowed RODC Password Replication Group"
+    	    "S-1-5- 21 domain -572"      = "Denied RODC Password Replication Group"
+	        "S-1-5-32-573"               = "BUILTIN\Event Log Readers"
+	        "S-1-5-32-574"               = "BUILTIN\Certificate Service DCOM Access"
+    	    "S-1-5-21-domain-522"        = "Cloneable Domain Controllers"
+	        "S-1-5-32-575"               = "BUILTIN\RDS Remote Access Servers"
+	        "S-1-5-32-576"               = "BUILTIN\RDS Endpoint Servers"
+    	    "S-1-5-32-577"               = "BUILTIN\RDS Management Servers"
+	        "S-1-5-32-578"               = "BUILTIN\Hyper-V Administrators"
+	        "S-1-5-32-579"               = "BUILTIN\Access Control Assistance Operators"
+    	    "S-1-5-32-580"               = "BUILTIN\Remote Management Users" }
+    }
+            
+    Function Collect-DCOM
+    {
+        $D = @( "" , "Access" , "Launch" | % { "Win32_DCOMApplication$_" } )
+        1..2 | % { $D[$_] = "$( $D[$_] )AllowedSetting" }
+        Return $D
+    }
+
+    Function Query-DCOM
+    {
+        [ CmdLetBinding () ] Param (
+            
+            [ Parameter ( Position = 0 , Mandatory = $False , ValueFromPipeline = $True ) ] [ String ] $GUID )
+
+        # Get the Default SIDS
+
+            $S = @( Collect-DefaultSID )
+            $S | % { $Keys = @( $_.Keys ) ; $Values = @( $_.Values ) ; $Count = @( $_.Count ) }
+
+        # Get the DCOM WMI Filters
+
+            $D = @( Collect-DCOM ) 
+
+            Echo "Querying WMI Hive"
+        
+            $G = [ Ordered ]@{ 
+                "$( $D[0] )" = @( GWMI $D[0] | ? { $_.AppID   -like "*$GUID*" } | Select Name, AppID | Sort Name )
+                "$( $D[1] )" = @( GWMI $D[1] | ? { $_.Element -like "*$GUID*" } | Select @{ Name = "SID" ; Expression = { ( $_.Setting.Split( '=' )[-1] ).Replace( '"' , '' ) } } )
+                "$( $D[2] )" = @( GWMI $D[2] | ? { $_.Element -like "*$GUID*" } | Select @{ Name = "SID" ; Expression = { ( $_.Setting.Split( '=' )[-1] ).Replace( '"' , '' ) } } ) }
+
+            $Section = ( "  " * 30 )
+
+            $G | % { If ( $G[0] -eq $Null ) { Echo "The AppID was not found." ; Break } }
+
+            $G[0] | % { $Section , "      Application : $( $_.Name ) " , $Section | % { Echo $_ }
+            
+            ForEach ( $x in 1..2 )
+            {
+                $C = $G[$x]
+
+                If ( $x -eq 1 ) { $Head = "Access" } 
+                If ( $x -eq 2 ) { $Head = "Launch" }
+
+                If ( ( $C ).Count -gt 0 )
+                {
+                    ForEach ( $y in ( $C ).SID )
+                    {
+                        ForEach ( $i in 0..( $Keys.Count - 1 ) )
+                        {
+                            $Keys[$i] | ? { $y -eq $_ } | % { 
+                                    
+                            "     $Head SID(s): $( $Values[$i] ) " , 
+                            "              SID : $y " , 
+                            $Section | % { Echo $_ } } 
+                        }
+                    }
+                }
+            }
+        }
+    }
