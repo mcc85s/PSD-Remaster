f4bef9bf4205b58edb09b4d54e90796e1838fde5
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-09-13 13:42:56 -0400

    Update Write-Echo.ps1
    Added some flair.

--- Write-Echo.ps1
+++ Write-Echo.ps1
@@ -1,157 +1,144 @@
     Function Write-Echo
     {
-        [ CmdLetBinding () ] Param (
-
+        [ CmdLetBinding ( ) ] Param (
             [ Parameter ( Position = 0 , Mandatory = $True , ValueFromPipeline = $True , ParameterSetName = "0" ) ][ Switch ]   $Action ,
             [ Parameter ( Position = 0 , Mandatory = $True , ValueFromPipeline = $True , ParameterSetName = "1" ) ][ Array ]     $Array ,
             [ Parameter ( Position = 0 , Mandatory = $True , ValueFromPipeline = $True , ParameterSetName = "2" ) ][ PSCustomObject ] $Table ,
-
             [ Parameter ( Position = 1 , Mandatory = $True , ParameterSetName = "0" ) ][ String ] $Type ,
             [ Parameter ( Position = 2 , Mandatory = $True , ParameterSetName = "0" ) ][ String ] $Info ,
-
             [ Parameter ( Position = 1 , ParameterSetName = "1" ) ]
-            [ Parameter ( Position = 1 , ParameterSetName = "2" ) ][ Switch ] $Wrap )
+            [ Parameter ( Position = 1 , ParameterSetName = "2" ) ][ Switch ] $Wrap ,
+            [ ValidateSet ( 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 ) ] 
+            [ Parameter ( Position = 2 , ParameterSetName = "0" ) ]
+            [ Parameter ( Position = 2 , ParameterSetName = "1" ) ]
+            [ Parameter ( Position = 2 , ParameterSetName = "2" ) ][ Alias ( "F" )][ String ] $ForegroundColor ,
+            [ ValidateSet ( 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 ) ] 
+            [ Parameter ( Position = 3 , ParameterSetName = "0" ) ]
+            [ Parameter ( Position = 3 , ParameterSetName = "1" ) ]
+            [ Parameter ( Position = 3 , ParameterSetName = "2" ) ][ Alias ( "B" )][ String ] $BackgroundColor )
 
         Begin 
         {
-            $B  = @{ 0 = 0..1 ; 1 = 1..0 } ; $Z  = "  " , "¯-" , "-_" | % { $_ * 54 } ; $S  = " // " , " \\ " ; $F  = "/¯¯\" , "\__/"
-            $FR = 0..1 | % { ( $F[( $B[$_] )] ) * 13 -join '' } ; $D  = 0..1 | % { $X , $Y = $B[$_][0..1] ; "$( $S[$X] + $FR[$_] + $F[$X] + $S[$Y] )" }
-            $L , $R = $S , $S[1..0] ; $LI = 0..1 | % { $L[$_] + $Z[0] + $R[$_] }
-            $TopL = "  $( "_" * 112 )  " , $D , " //$( "¯" * 110 )\\ " , $LI[1]
-            $BotL = $LI[0] , " \\$( "_" * 110 )// " , $D , "  $( "¯" * 112 )  "
-            $OP = @( )
+            $B = @{ 0 = 0..1 ; 1 = 1..0 } ; $Z = "  " , "¯-" , "-_" | % { $_ * 54 } ; $S = " // " , " \\ " ; $F = "/¯¯\" , "\__/"
+            $OP = @( ) ; $FR = 0..1 | % { ( $F[( $B[$_] )] ) * 13 -join '' } 
+            $D = 0..1 | % { $X , $Y = $B[$_][0..1] ; "$( $S[$X] + $FR[$_] + $F[$X] + $S[$Y] )" }
+            $L , $R = $S , $S[1..0] ; $LI = 0..1 | % { $L[$_] + $Z[0] + $R[$_] } 
         }
 
         Process 
-        {
-            # - [ Top Wrapper ] - - - - - - - - #
-
-            If ( $Wrap ) { $OP += $TopL }
-
-            # - [ Action Wrapper ]- - - - - - - #
-
-            If ( $Action )
-            {
-                $T , $I = $Type , $Info ; $ST , $SI = ( 25 - $T.Length ) , ( 80 - $I.Length ) | % { " " * $_ }
-                $Sub = "$( $S[1] )$ST$T : $I$SI$( $S[0] )" ; $AT , $AB = 1..2 | % { $S[0] + $Z[$_] + $S[1] }
-                $OP += "" , $AT , $Sub , $AB , ""
+        {   
+            If (   $Wrap ) 
+            { 
+                $OP += "  $( "_" * 112 )  " , $D[0] , $D[1] , " //$( "¯" * 110 )\\ " , $LI[1] 
             }
 
-            # - [ Array Wrapper ] - - - - - - - #
+            If ( $Action ) 
+            {   
+                $T , $I = $Type , $Info ; $ST , $SI = ( 25 - $T.Length ) , ( 80 - $I.Length ) | % { " " * $_ }
+                $Sub = "$( $S[1] )$ST$T : $I$SI$( $S[0] )" ; $AT , $AB = 1..2 | % { $S[0] + $Z[$_] + $S[1] }
+                $OP += "" , $AT , $Sub , $AB , "" 
+            }
 
-            If ( $Array ) { Return $Array }
+            #     Array Wrapper
 
-            # - [ Hashtable Wrapper ] - - - - - #
+            If (  $Array ) 
+            { 
+                $Array | % { $OP += $_ }
+            } 
 
-            If ( $Table )
-            {
-                # - [ Discover / Format Title ] - - - - - - #
+            # Hashtable Wrapper
 
-                $HT       = $Hash | GM
-                $Title    =   $HT | ? { $_.Name -eq "[+]Title" } | % { $_.Definition.Split( "=" )[1] } | % { $_.Replace( ' ' , '-' ) } | % { "[ $_ ]" }
+            If (  $Table ) 
+            {   
+                $Index = @( ) ; $Section = @( ) ; $Name = $Null ; $Item = $Null ; $C = $Null
 
-                # - [ Calculate Title via Shrodingers Fricken' Cat ] - - - - - - - #
+                $ID = $Table | GM | ? { $_.MemberType -eq "NoteProperty" } 
 
-                $TT = 108 - $Title.Length ; $TX = $TT % 4 ; $TY = ( $TT - $TX ) / 4 ; $T = ( $Title | % { "$_" , "$_-" , "$_ " , " $_ -" } )[$TX]
+                # [ Title ]
+                
+                $ID | ? { $_.Name -eq "Class" } | % { $Title = $Table.$( $_.Name ) }
+
+                $Title = $Title.Replace( ' ' , '-' ) | % { "[ $_ ]" } 
+
+                $TT = 108 - $Title.Length ; $TX = $TT % 4 ; $TY = ( $TT - $TX ) / 4
+                
+                $T = ( $Title | % { "$_" , "$_-" , "$_ " , " $_ -" } )[ $TX ]
+                
                 $TL , $TR = "_¯" , "¯_" | % { $_ * $TY }
+                
+                $Title = $S[0] + $TL + $T + $TR + $S[1]
 
-                $Title    = $S[0] + $TL + $T + $TR + $S[1]
+                $OP += $Title , $LI[1]
+                
+                # [ Sections / Keys ]
 
-                # - [ Send Title to Output Array ] - - - - - #
+                $ID | ? { $_.Name -like      "*ID:*" } | % { $Index   += $Table.$( $_.Name ) }
+                $ID | ? { $_.Name -like "*Section:*" } | % { $Section += $Table.$( $_.Name ) }
 
-                $OP      += $Title , $LI[1]
-
-                # - [ Discover / Format Sections ] - - - - - - #
-
-                $Index    =   $HT | ? { $_.Name -like "*]Index*" }
-                $Count    = $Index.Count - 1
+                $Count = $Index.Count - 1
+                
+                $Total = 0
 
                 If ( $Count -gt 0 ) { $Count = 0..$Count }
 
-                $Names    = $Index | % { $_.Definition.Split( "=" )[1] }
+                $Count | % {
 
-                $Total    = 0
-
-                # - [ Mathematics and Logic having a baby... ] - - - - #
-
-                ForEach ( $i in $Count )
-                { 
-                    If ( $Count.Count -gt 1 )
-                    {
-                        If ( $C -ne $Null ) { $C = $C + 1 }
-                        If ( ( $C -eq $Null ) -and ( $Name -eq $Null ) ) { $C = 0 }
-
-                        $Name = $Names[$i]
+                    If ( $Index.Count -gt 1 )
+                    {   
+                        If   ( $C -ne $Null )                            { $C = $C + 1 }
+                        If ( ( $C -eq $Null ) -and ( $Name -eq $Null ) ) { $C =      0 }
+                        $Name = $Index[$C] ; $Item = $Section[$C]
                     }
 
-                    If ( $Count.Count -eq 1 ) 
-                    {
-                        $Name = $Names
-                    }
+                    If ( $Index.Count -eq 1 ) { $Name = $Index ; $Item = $Section } 
 
-                    # - [ Calculate / Output Section:# ] - - - - - - #
-
-                    $U  = 98 - $Name.Length
-                    $V  = $Total % 2
-                    $W  = ( 1 - $V )
-
+                    $U = 98 - $Name.Length
+                    $V = $Total % 2
+                    $W = ( 1 - $V ) 
+                    
                     $SHeader = "_" , "¯" | % { "$( $L[$V] + ( "$_" * 108 ) + $R[$V] )" }
-                    $SLine   = "$( $L[$W] + ( "-" * 10 ) + $Name + ( "-" * $U ) + $R[$W] )"
-
-                    $OP += $SHeader[0] , $SLine , $SHeader[1]
-
-                    $Total = $Total + 3
-
-                    # - [ Calculate / Parse Items:# ] - - - - - - #
-
-                    $X = "[$I]:"
-                    $C = 0
-                    $HT | ? { "$( $_.Name.Split( ':' )[0] ):" -eq $X } | % {
-
-                        $DF    = $_.Definition.Replace( "string $X" , "" ).Split( "=" )
                     
-                        $Y     = $Total % 2
-
-                        # - [ Format Key Length ] - #
-
-                        $Key   = $DF[0].Split(':')[1]
-
-                        If ( $Key.Length -gt 20 ) 
-                        { 
-                            $Key   = "$( $DF[0].SubString( 0 , 20 ) ) ... " 
-                        }
-                        
-                        Else 
-                        {   
-                            $Key   = $Key | % { "$( " " * ( 25 - $_.Length ) )$_" } 
-                        }
-
-                        # - [ Format Value Length ] - #
-
-                        If ( $DF[1].Length -gt 75 ) 
-                        { 
-                            $Value = "$( $DF[1].SubString( 0 , 75 ) ) ... " 
-                        }
-
-                        Else 
-                        { 
-                            $Value = $DF[1] | % { "$_$( " " * ( 80 - $_.Length ) )" } 
-                        }
+                    $SLine   = $L[$W] + ( "-" * 10 ) + $Name + ( "-" * $U ) + $R[$W]
                     
-                        $OP   += "$( $L[$Y] + $Key ) : $( $Value + $R[$Y] )"
-                        $Total++
-                        $C++
-                    }
-                }     
+                    $OP     += $SHeader[0] , $SLine , $SHeader[1] 
+                    
+                    $Total   = $Total + 3
+
+                    $BX       = 0
+
+                    ForEach ( $IX in ( 0..( $Item.Count - 1 ) ) )
+                    {
+                        $Item."Item:$IX" | % { $Key = $_.ID ; $Value = $_.Value }
+
+                        $Y = $Total % 2
+
+                        If (   $Key.Length -gt 20 ) {   $Key = $Key.SubString( 0 , 20 ) + " ... " } 
+                        Else                        {   $Key = $Key | % { "$( " " * ( 25 - $_.Length ) )$_" } }
+                        If ( $Value.Length -gt 75 ) { $Value = $Value.SubString( 0 , 75 ) + " ... " } 
+                        Else                        { $Value = $Value | % { "$_$( " " * ( 80 - $_.Length ) )" } }
+
+                        $OP += "$( $L[$Y] + $Key ) : $( $Value + $R[$Y] )"
+
+                        $Total ++
+
+                        $BX ++ 
+                    } 
+                } 
             }
 
             If ( $Wrap ) 
             { 
-                If ( ( $OP.Count | Select -Last 1 ) % 2 -ne 1 ) { $OP += $LI[0] } 
+                If ( ( $OP.Count | Select -Last 1 ) % 2 -ne 0 ) { $OP += $LI[0] } 
                 
-                $OP += " \\$( "_" * 110 )// " , $D , "  $( "¯" * 112 )  " 
+                $OP += " \\$( "_" * 110 )// " , $D[0] , $D[1] , "  $( "¯" * 112 )  " 
             }
         }
 
-        End { Return $OP }
-}
+        End 
+        {
+            If (   $ForegroundColor -ne $Null ) { If ( $BackgroundColor -eq $Null )   { $OP | % { Write-Host -F $FG $_ } } }
+            If (   $BackgroundColor -ne $Null ) { If ( $ForegroundColor -eq $Null )   { $OP | % { Write-Host -B $BG $_ } } }
+            If ( ( $ForegroundColor -ne $Null ) -and ( $BackgroundColor -ne $Null ) ) { $OP | % { Write-Host -F $FG -B $BG $_ } }
+            If ( ( $ForegroundColor -eq $null ) -and ( $BackgroundColor -eq $null ) ) { $OP | % { Write-Host $_ } }
+        }
+    }
