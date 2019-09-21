395efb73bb117f4564f23b290eb7e3caede6ae2a
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-09-21 15:52:27 -0400

    Update PSDGather.psm1
    Did some stuff.

--- Scripts/PSDGather.psm1
+++ Scripts/PSDGather.psm1
@@ -1,23 +1,3 @@
-#\\- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//#
-#// /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ \\#
-#\\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ //#
-#// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \\#
-#\\                                                                                                                   //#
-#//   <@[ Script-Initialization ]@>                        "Script Magistration by Michael C. 'Boss Mode' Cook Sr."   \\#
-#\\                                                                                                                   //#
-#//                        [ Secure Digits Plus LLC | Hybrid ] [ Desired State Controller ]                           \\#
-#\\                                                                                                                   //#
-#//                  [ https://www.securedigitsplus.com | Server/Client | Seedling/Spawning Script ]                  \\#
-#\\                                                                                                                   //#
-#//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-#\\ - - [ PXD-Gather ]- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //#
-# # #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-
-# The following script is newly formatted and contains slight alterations or adjustments made by the aforementioned auth.
-# Although I am nowhere close to complete, I have given these scripts my full attention in attempting to optimize them.
-# There are definitely many issues that I have noticed, but making mistakes is part of life. Learning from them, and
-# making the effort to correct them is what matters the most. Comments, questions, mcook@securedigitsplus.com
-
 # // ***************************************************************************
 # // 
 # // PowerShell Deployment for MDT
@@ -39,14 +19,11 @@ Import-Module PSDGather
 $VerbosePreference = "Continue"
 
 #Write-Verbose -Message "$($MyInvocation.MyCommand.Name): Load core module"
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Load core modules"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Load core modules"
 
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Deployroot is now $( $TSenv:DeployRoot)"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Deployroot is now $( $TSenv:DeployRoot)"
 
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: env:PSModulePath is now $Env:PSModulePath"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): env:PSModulePath is now $Env:PSModulePath"
 
 Write-PSDEvent -MessageID 41000 -severity 1 -Message "Starting: $( $MyInvocation.MyCommand.Name )"
 
