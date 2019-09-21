b9e4e9f613cebebca5a67694e4b075a05ff64f78
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-09-21 15:55:41 -0400

    Update PSDNextPhase.ps1
    Stuff that happens, may or may not be cool. So..? It is what it is.

--- Scripts/PSDNextPhase.ps1
+++ Scripts/PSDNextPhase.ps1
@@ -1,23 +1,3 @@
-#\\- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//#
-#// /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ \\#
-#\\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ //#
-#// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \\#
-#\\                                                                                                                   //#
-#//   <@[ Script-Initialization ]@>                        "Script Magistration by Michael C. 'Boss Mode' Cook Sr."   \\#
-#\\                                                                                                                   //#
-#//                        [ Secure Digits Plus LLC | Hybrid ] [ Desired State Controller ]                           \\#
-#\\                                                                                                                   //#
-#//                  [ https://www.securedigitsplus.com | Server/Client | Seedling/Spawning Script ]                  \\#
-#\\                                                                                                                   //#
-#//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-#\\ - - [ PXD-NextPhase ] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //#
-# # #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-
-# The following script is newly formatted and contains slight alterations or adjustments made by the aforementioned auth.
-# Although I am nowhere close to complete, I have given these scripts my full attention in attempting to optimize them.
-# There are definitely many issues that I have noticed, but making mistakes is part of life. Learning from them, and
-# making the effort to correct them is what matters the most. Comments, questions, mcook@securedigitsplus.com
-
 # // ***************************************************************************
 # // 
 # // PowerShell Deployment for MDT
@@ -32,20 +12,15 @@
 Param ( )
 
 # Load core modules
-Import-Module Microsoft.BDD.TaskSequenceModule -Scope Global
+Import-Module Microsoft.BDD.TaskSequenceModule -Scope Global # <- This thing is still encrypted
 Import-Module PSDUtility
 Import-Module PSDDeploymentShare
 
 $verbosePreference = "Continue"
 
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Load core modules"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Deployroot is now $($tsenv:DeployRoot)"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: env:PSModulePath is now $env:PSModulePath"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Load core modules"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Deployroot is now $($tsenv:DeployRoot)"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): env:PSModulePath is now $env:PSModulePath"
 
 #Next Phase
 $PHASE = $tsenv:PHASE
@@ -64,13 +39,8 @@ Switch ( $PHASE )
 
 $tsenv:PHASE = $Phase
 
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: --------------------"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Next Phase is $PHASE"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Save all the current variables for later use"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): --------------------"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Next Phase is $PHASE"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Save all the current variables for later use"
 
 Save-PSDVariables
