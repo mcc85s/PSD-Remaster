64db8b63f116cb94f3892787099c399316115ae5
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-09-21 16:51:02 -0400

    Update PSDValidate.ps1

--- Scripts/PSDValidate.ps1
+++ Scripts/PSDValidate.ps1
@@ -1,23 +1,3 @@
-#\\- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//#
-#// /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ \\#
-#\\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ //#
-#// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \\#
-#\\                                                                                                                   //#
-#//   <@[ Script-Initialization ]@>                        "Script Magistration by Michael C. 'Boss Mode' Cook Sr."   \\#
-#\\                                                                                                                   //#
-#//                        [ Secure Digits Plus LLC | Hybrid ] [ Desired State Controller ]                           \\#
-#\\                                                                                                                   //#
-#//                  [ https://www.securedigitsplus.com | Server/Client | Seedling/Spawning Script ]                  \\#
-#\\                                                                                                                   //#
-#//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-#\\ - - [ PXD-Validate ]- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //#
-# # #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-
-# The following script is newly formatted and contains slight alterations or adjustments made by the aforementioned auth.
-# Although I am nowhere close to complete, I have given these scripts my full attention in attempting to optimize them.
-# There are definitely many issues that I have noticed, but making mistakes is part of life. Learning from them, and
-# making the effort to correct them is what matters the most. Comments, questions, mcook@securedigitsplus.com
-
 # // ***************************************************************************
 # // 
 # // PowerShell Deployment for MDT
@@ -32,32 +12,19 @@
 Param ( )
 
 # Load core modules
-Import-Module Microsoft.BDD.TaskSequenceModule -Scope Global
+Import-Module Microsoft.BDD.TaskSequenceModule -Scope Global # <- This thing is still encrypted
 Import-Module PSDUtility
 Import-Module PSDDeploymentShare
 
 $verbosePreference = "Continue"
 
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Load core modules"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Deployroot is now $( $tsenv:DeployRoot )"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: env:PSModulePath is now $env:PSModulePath"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: tsenv:ImageSize $( $tsenv:ImageSize )"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: tsenv:ImageProcessorSpeed $( $tsenv:ImageProcessorSpeed )"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: tsenv:ImageMemory $( $tsenv:ImageMemory )"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name)
-: tsenv:VerifyOS $( $tsenv:VerifyOS )"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Load core modules"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Deployroot is now $( $tsenv:DeployRoot )"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): env:PSModulePath is now $env:PSModulePath"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): tsenv:ImageSize $( $tsenv:ImageSize )"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): tsenv:ImageProcessorSpeed $( $tsenv:ImageProcessorSpeed )"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): tsenv:ImageMemory $( $tsenv:ImageMemory )"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name): tsenv:VerifyOS $( $tsenv:VerifyOS )"
 
 <#
     '//----------------------------------------------------------------------------
@@ -79,9 +46,9 @@ If ( $TSEnv:DeploymentType -eq "REFRESH" )
         }
     }
 
-	If ( $TSEnv:VerifyOS -eq "SERVER" )
+    If ( $TSEnv:VerifyOS -eq "SERVER" )
     {
-		If ( $TSEnv:IsServerOS -eq "FALSE" )
+	If ( $TSEnv:IsServerOS -eq "FALSE" )
         {
             $Message = "ERROR - Attempting to deploy a server operating system to a machine running a client operating system."
             Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): $Message" -LogLevel 3
