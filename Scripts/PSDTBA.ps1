1a007c1f324d97f81b28de93669be295e01c8d20
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-09-21 16:28:30 -0400

    Update PSDTBA.ps1

--- Scripts/PSDTBA.ps1
+++ Scripts/PSDTBA.ps1
@@ -1,23 +1,3 @@
-#\\- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//#
-#// /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ \\#
-#\\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ \/ /\ //#
-#// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \\#
-#\\                                                                                                                   //#
-#//   <@[ Script-Initialization ]@>                        "Script Magistration by Michael C. 'Boss Mode' Cook Sr."   \\#
-#\\                                                                                                                   //#
-#//                        [ Secure Digits Plus LLC | Hybrid ] [ Desired State Controller ]                           \\#
-#\\                                                                                                                   //#
-#//                  [ https://www.securedigitsplus.com | Server/Client | Seedling/Spawning Script ]                  \\#
-#\\                                                                                                                   //#
-#//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-#\\ - - [ PXD-TBA ] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //#
-# # #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\\#
-
-# The following script is newly formatted and contains slight alterations or adjustments made by the aforementioned auth.
-# Although I am nowhere close to complete, I have given these scripts my full attention in attempting to optimize them.
-# There are definitely many issues that I have noticed, but making mistakes is part of life. Learning from them, and
-# making the effort to correct them is what matters the most. Comments, questions, mcook@securedigitsplus.com
-
 # // ***************************************************************************
 # // 
 # // PowerShell Deployment for MDT
@@ -31,24 +11,16 @@
 
 Param ( )
 
-Import-Module Microsoft.BDD.TaskSequenceModule -Scope Global
+Import-Module Microsoft.BDD.TaskSequenceModule -Scope Global # <- This thing is still encrypted
 Import-Module PSDUtility
 Import-Module PSDDeploymentShare
 
 $verbosePreference = "Continue"
 
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Load core modules"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: Deployroot is now $( $tsenv:DeployRoot )"
-
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: env:PSModulePath is now $env:PSModulePath"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Load core modules"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): Deployroot is now $( $tsenv:DeployRoot )"
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): env:PSModulePath is now $env:PSModulePath"
 
 # Notify
-Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name )
-: The built in VB Script has been replaced by the script, 
-however, the function the VB Script would have done is not
- yet implemented, sorry, working on this" 
-
+Write-PSDLog -Message "$( $MyInvocation.MyCommand.Name ): The built in VB Script has been replaced by the script, 
+however, the function the VB Script would have done is not yet implemented, sorry, working on this" # Well... apology accepted. 
