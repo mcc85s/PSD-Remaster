07056c2a4684e81c798dcc6c1c899b1c4e04565e
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-10-06 21:41:09 -0400

    Create Install-HybridDSC.ps1

---
+++ Modules/Hybrid-DSC/Install-HybridDSC.ps1
@@ -0,0 +1,193 @@
+# ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯   //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//  [ Secure Digits Plus LLC | Hybrid | Desired State Controller ]  \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯       _____________________________________________________        ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯        __/¯¯\__[ Dynamically Engineered Digital Security ]__/¯¯\__         ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\  _________________________ ________________ ___________________________________  //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__// | Application Development | Virtualization | Network and Hardware Magistration | \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\  ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//   https://www.securedigitsplus.com | Server-Client | Seedling-Spawning Script    \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\___¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   ¯¯¯¯¯¯¯¯¯¯¯¯¯   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ___//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   [ Provisional Author : Michael C Cook Sr. | "The Buck Stops Here" ]    //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//   ____    ____    ____    ____    ____    ____    ____    ____    ____   \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+#//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯  
+#\\  [ Install-HybridDSC ] @: Installs the module required to provision all aspects of Hybrid-DSC
+#//   ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____      
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\___  
+#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+# ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯          
+    Using Namespace System.Security.Principal
+    Using Namespace System.Management.Automation
+
+    $CS , $OS = "Computer" , "Operating" | % { GCIM Win32_$( $_ )System }
+    
+    "Windows"   | % { IEX "( [ $_`Principal ][ $_`Identity ]::GetCurrent() ).IsInRole( 'Administrator' )" } | ? {
+        $False  | ? { [ Int ] $OS.BuildNumber -ge 6000 }
+        $True   | % { $MyInvocation | % { SAPS PowerShell -Verb Runas -Args "-File `"$( $_.MyCommand.Path ) $( $_.UnboundArguments )" } } } 
+
+#//¯¯\\__________________________________________________________________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+    Function Convert-XAMLToWindow # Modified, originally by Dr. Tobias Weltner    ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
+    {#\______________________________________________________________________________/¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯      
+
+        Param ( [ Parameter ( Mandatory ) ] [ String ] $XAML , [ String [] ] $NE = $Null , [ Switch ] $PassThru )
+
+        @( "Framework" , "Core" | % { "Presentation$_" } ) + "WindowsBase" | % { Add-Type -AssemblyName $_ }
+ 
+        $NR = [ XML.XMLReader ]::Create( [ IO.StringReader ] $XAML ) ; $OP = [ Windows.Markup.XAMLReader ]::Load( $NR )
+        
+        $NE | % { $OP | Add-Member -MemberType NoteProperty -Name $_ -Value $OP.FindName( $_ ) -Force }
+ 
+        If ( $PassThru ) { $OP } Else { $Null = $GUI.Dispatcher.InvokeAsync( { $OP = $GUI.ShowDialog() ; SV -Name OP -Value $OP -Scope 1 } ).Wait() ; $OP }
+
+    }#                                                                            ____    ____    ____    ____    ____    ____    ____    ____    ____  
+#//¯¯\\__________________________________________________________________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+    Function Show-WPFWindow # Modified, originally by Dr. Tobias Weltner          ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
+    {#\______________________________________________________________________________//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯      
+        
+        Param ( [ Parameter ( Mandatory ) ] [ Windows.Window ] $GUI )
+
+        $OP = $Null ; $Null = $GUI.Dispatcher.InvokeAsync( { $OP = $GUI.ShowDialog() ; SV -Name OP -Value $OP -Scope 1 } ).Wait() ; $OP 
+
+    }#                                                                            ____    ____    ____    ____    ____    ____    ____    ____    ____ 
+#//¯¯\\__________________________________________________________________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+    Function Get-NetworkDrive # Establishes access to a local network drive       ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
+    {#\______________________________________________________________________________//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯      
+
+        $Xaml = @"
+<Window xmlns   = "http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x = "http://schemas.microsoft.com/winfx/2006/xaml"
+        Title = "Secure Digits Plus LLC | Hybrid-DSC Module Installation" Height = "400" Width = "450" >
+    <GroupBox Header = "Access Drive" Margin = "10" >
+        <Grid>
+            <Grid.RowDefinitions>
+                <RowDefinition Height = "4*" />
+                <RowDefinition Height =  "*" />
+            </Grid.RowDefinitions>
+            <Grid Grid.Row = "0" >
+                <Grid.ColumnDefinitions>
+                    <ColumnDefinition Width =    "*" />
+                    <ColumnDefinition Width = "2.5*" />
+                </Grid.ColumnDefinitions>
+                <Grid.RowDefinitions>
+                    <RowDefinition Height = "*"/>
+                    <RowDefinition Height = "*"/>
+                    <RowDefinition Height = "*"/>
+                    <RowDefinition Height = "*"/>
+                    <RowDefinition Height = "*"/>
+                    <RowDefinition Height = "*"/>
+                </Grid.RowDefinitions>
+                <TextBlock                         Grid.Row = "0" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Drive Name</TextBlock>
+                <TextBox      Name = "Name"        Grid.Row = "0" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
+                <TextBlock                         Grid.Row = "1" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Network Path</TextBlock>
+                <TextBox      Name = "Root"        Grid.Row = "1" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
+                <TextBlock                         Grid.Row = "2" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Description</TextBlock>
+                <TextBox      Name = "Description" Grid.Row = "2" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
+                <TextBlock                         Grid.Row = "3" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >User Name</TextBlock>
+                <TextBox      Name = "Username"    Grid.Row = "3" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
+                <TextBlock                         Grid.Row = "4" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Password</TextBlock>
+                <PasswordBox  Name = "Password"    Grid.Row = "4" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" PasswordChar  =     "*" ></PasswordBox>
+                <TextBlock                         Grid.Row = "5" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Confirm</TextBlock>
+                <PasswordBox  Name = "Confirm"     Grid.Row = "5" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" PasswordChar  =     "*" ></PasswordBox>
+            </Grid>
+            <Grid Grid.Row = "1" >
+                <Grid.ColumnDefinitions>
+                    <ColumnDefinition Width = "*" />
+                    <ColumnDefinition Width = "*" />
+                </Grid.ColumnDefinitions>
+                <Button Grid.Column = "0" Name =     "Ok" Content = "Ok"     Margin = "15"/>
+                <Button Grid.Column = "1" Name = "Cancel" Content = "Cancel" Margin = "15"/>
+            </Grid>
+        </Grid>
+    </GroupBox>
+</Window>
+"@
+    $NamedElements = "Name" , "Root" , "Description" , "Username" , "Password" , "Confirm" , "Ok" , "Cancel"
+
+    $GUI = Convert-XAMLtoWindow -Xaml $Xaml -NE $NamedElements -PassThru
+    
+    $GUI.Cancel.Add_Click({  $GUI.DialogResult = $False })
+
+    $GUI.Ok.Add_Click({
+
+        $0 = @( "Drive Label" , "Root Folder" , "UserName" , "Password" , "Confirmation" )
+        $1 = $0 | % { "$_ Error" } ; $2 = $0 | % { "Invalid $_" }
+
+    $MSG = 0..4 | % { "[ System.Windows.MessageBox ]::Show( '$( $1[$_] )' , '$( $2[$_] )' )" }
+
+    If     ( $GUI.Name.Text -eq "" )                                   { IEX $MSG[0] }
+    ElseIf ( $GUI.Root.Text -eq "" )                                   { IEX $MSG[1] }
+    ElseIf ( $GUI.Username.Text -eq "" )                               { IEX $MSG[2] }
+    ElseIf ( $GUI.Password.Password -eq "" )                           { IEX $MSG[3] }
+    ElseIf ( $GUI.Confirm.Password  -eq "" )                           { IEX $MSG[4] }
+    ElseIf ( $GUI.Password.Password  -notmatch  $GUI.Confirm.Password) { IEX $MSG[4] }
+
+    Else { $GUI.DialogResult = $True }
+
+    })
+
+        $Null = $GUI.Name.Focus()
+
+        $OP = Show-WPFWindow -GUI $GUI
+
+        If ( $OP -eq $True )
+        { 
+            IEX "Using Namespace System.Management.Automation"
+            $Cred = [ PSCredential ]::New( $GUI.Username.Text , $GUI.Password.SecurePassword )
+
+            $PSDrive = @{ Name        = $GUI.Name.Text
+                          PSProvider  = "FileSystem"
+                          Root        = $GUI.Root.Text
+                          Description = $( $GUI.Description | ? { If ( $_ -ne $Null ) { $_ } Else { "" } } )
+                          Credential  = $Cred }
+
+            NDR @PSDrive
+        }
+
+        Else { Write-Warning "Either the user cancelled, or the dialogue failed"  }
+        
+    }#                                                                            ____    ____    ____    ____    ____    ____    ____    ____    ____ 
+#//¯¯\\__________________________________________________________________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+    Function Install-HybridDSC #                                                  ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
+    {#\______________________________________________________________________________/¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯      
+        
+        $Path     = "$( $ENV:PSModulePath.Split( ';' ) | ? { $_ -like "*$ENV:Username*" } )\Hybrid-DSC".Split( '\' )
+        
+        $Tree     = $Path[0]
+
+        $X        = 1
+
+        $Path     | % {
+
+            $Tree = "$Tree\$( $Path[$X] )"
+
+            If ( ( Test-Path $Tree ) -eq $False )
+            {
+                NI $Tree -ItemType Directory
+               
+            }
+
+            $X    = $X + 1
+        }
+
+        $Drive    = Get-NetworkDrive | % { $_.Root }
+
+        $Module   = GCI "$Drive\Modules" -Recurse "*-DSC.ps*" | % { $_.FullName }
+
+        $Module   | % { CP $_ $Tree }
+
+        $Tree     | % { GCI $_ -Recurse "*Hybrid-DSC.PS1*" } | % { IPMO $_.FullName ; If ( $? -eq $True ) { Write-Theme -Foot } }
+
+    }#                                                                            ____    ____    ____    ____    ____    ____    ____    ____    ____  
+#//¯¯\\__________________________________________________________________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+#\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+        Install-HybridDSC                                                        #¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
+     #\______________________________________________________________________________//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯      
+     #¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯                                                                   
