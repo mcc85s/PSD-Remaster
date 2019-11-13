f45dab2d5181b5e6bbf1f851d447139cb704cf24
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-11-13 14:28:38 -0500

    Update Install-HybridDSC.ps1

--- Modules/Hybrid-DSC/Install-HybridDSC.ps1
+++ Modules/Hybrid-DSC/Install-HybridDSC.ps1
@@ -1,27 +1,27 @@
-# ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯   //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//  [ Secure Digits Plus LLC | Hybrid | Desired State Controller ]  \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯       _____________________________________________________        ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯        __/¯¯\__[ Dynamically Engineered Digital Security ]__/¯¯\__         ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\  _________________________ ________________ ___________________________________  //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__// | Application Development | Virtualization | Network and Hardware Magistration | \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\  ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//   https://www.securedigitsplus.com | Server-Client | Seedling-Spawning Script    \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\___¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   ¯¯¯¯¯¯¯¯¯¯¯¯¯   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ___//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   [ Provisional Author : Michael C Cook Sr. | "The Buck Stops Here" ]    //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//   ____    ____    ____    ____    ____    ____    ____    ____    ____   \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-#//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯  
-#\\  [ Install-HybridDSC ] @: Installs the module required to provision all aspects of Hybrid-DSC
-#//   ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____      
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\___  
-#//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
-#\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
-# ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯          
-    Using Namespace System.Security.Principal
-    Using Namespace System.Management.Automation
+<#___ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____ -- ____  
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯   //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//  [ Secure Digits Plus LLC | Hybrid | Desired State Controller ]  \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯       _____________________________________________________        ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯        __/¯¯\__[ Dynamically Engineered Digital Security ]__/¯¯\__         ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\  _________________________ ________________ ___________________________________  //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__// | Application Development | Virtualization | Network and Hardware Magistration | \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\  ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__//   https://www.securedigitsplus.com | Server-Client | Seedling-Spawning Script    \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\___¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯   ¯¯¯¯¯¯¯¯¯¯¯¯¯   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ___//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\   [ Provisional Author : Michael C Cook Sr. | "The Buck Stops Here" ]    //¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//   ____    ____    ____    ____    ____    ____    ____    ____    ____   \\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\ 
+\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__// 
+//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯  
+\\  [ Install-HybridDSC ] @: Installs the module required to provision all aspects of Hybrid-DSC
+//   ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    ____    
+\\__//¯¯\\__//==\\__/----\__//==\\__/----\__//==\\__//¯¯\\__//==\\__/----\__//==\\__/---\\__//¯¯\\__/----\__//==\\__/----\__//==\\__/----\__//==\\___  
+//¯¯\\__________________________________________//¯¯\\__//¯¯\\___________________________¯¯¯¯    ¯¯¯¯ ¯¯ ¯¯¯¯ -- ¯¯¯¯ ¯¯ ¯¯¯¯ -- ¯¯¯¯ ¯¯ ¯¯¯¯ -- ¯¯¯\\ 
+\\__//¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\___    ____ -- ____ __ ____ -- ____ __ ____ -- ____ __ ___// 
+ ¯¯¯¯   [ Declare Namespaces & Load Modules ] __________________________________________//¯¯\\__//¯¯\\==//¯¯\----/¯¯\\==//¯¯\----/¯¯\\==//¯¯\----/¯¯¯  
+                                              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    #>
+    "Security.Principal" , "Management.Automation" | % { IEX "Using Namespace System.$_" } 
 
     $CS , $OS = "Computer" , "Operating" | % { GCIM Win32_$( $_ )System }
     
@@ -60,81 +60,81 @@
     Function Get-NetworkDrive # Establishes access to a local network drive       ¯¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
     {#\______________________________________________________________________________//¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯    ¯¯¯¯      
 
-        $Xaml = @"
-<Window xmlns   = "http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-        xmlns:x = "http://schemas.microsoft.com/winfx/2006/xaml"
-        Title = "Secure Digits Plus LLC | Hybrid-DSC Module Installation" Height = "400" Width = "450" >
-    <GroupBox Header = "Access Drive" Margin = "10" >
-        <Grid>
-            <Grid.RowDefinitions>
-                <RowDefinition Height = "4*" />
-                <RowDefinition Height =  "*" />
-            </Grid.RowDefinitions>
-            <Grid Grid.Row = "0" >
-                <Grid.ColumnDefinitions>
-                    <ColumnDefinition Width =    "*" />
-                    <ColumnDefinition Width = "2.5*" />
-                </Grid.ColumnDefinitions>
-                <Grid.RowDefinitions>
-                    <RowDefinition Height = "*"/>
-                    <RowDefinition Height = "*"/>
-                    <RowDefinition Height = "*"/>
-                    <RowDefinition Height = "*"/>
-                    <RowDefinition Height = "*"/>
-                    <RowDefinition Height = "*"/>
-                </Grid.RowDefinitions>
-                <TextBlock                         Grid.Row = "0" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Drive Name</TextBlock>
-                <TextBox      Name = "Name"        Grid.Row = "0" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
-                <TextBlock                         Grid.Row = "1" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Network Path</TextBlock>
-                <TextBox      Name = "Root"        Grid.Row = "1" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
-                <TextBlock                         Grid.Row = "2" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Description</TextBlock>
-                <TextBox      Name = "Description" Grid.Row = "2" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
-                <TextBlock                         Grid.Row = "3" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >User Name</TextBlock>
-                <TextBox      Name = "Username"    Grid.Row = "3" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" TextAlignment =  "Left" ></TextBox>
-                <TextBlock                         Grid.Row = "4" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Password</TextBlock>
-                <PasswordBox  Name = "Password"    Grid.Row = "4" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" PasswordChar  =     "*" ></PasswordBox>
-                <TextBlock                         Grid.Row = "5" Grid.Column = "0" Margin = "10" VerticalAlignment = "Center" TextAlignment = "Right" >Confirm</TextBlock>
-                <PasswordBox  Name = "Confirm"     Grid.Row = "5" Grid.Column = "1" Margin = "10" VerticalAlignment = "Center" PasswordChar  =     "*" ></PasswordBox>
-            </Grid>
-            <Grid Grid.Row = "1" >
-                <Grid.ColumnDefinitions>
-                    <ColumnDefinition Width = "*" />
-                    <ColumnDefinition Width = "*" />
-                </Grid.ColumnDefinitions>
-                <Button Grid.Column = "0" Name =     "Ok" Content = "Ok"     Margin = "15"/>
-                <Button Grid.Column = "1" Name = "Cancel" Content = "Cancel" Margin = "15"/>
-            </Grid>
-        </Grid>
-    </GroupBox>
-</Window>
-"@
-    $NamedElements = "Name" , "Root" , "Description" , "Username" , "Password" , "Confirm" , "Ok" , "Cancel"
+        
+        $Schema = "http://schemas.microsoft.com/winfx/2006/xaml" ; $Author = "Secure Digits Plus LLC"
 
-    $GUI = Convert-XAMLtoWindow -Xaml $Xaml -NE $NamedElements -PassThru
+        $Glossary  = 
+            "   `$W = 'Width'             " , "   `$H = 'Height'            " , "  `$MA = 'Margin'          " , 
+            "  `$HD = 'Header'            " , "  `$GB = 'GroupBox'          " , " `$CHK = 'CheckBox'        " , 
+            "  `$TB = 'TextBox'           " , " `$TBL = 'TextBlock'         " , "   `$G = 'Grid'            " , 
+            "  `$RD = 'RowDefinition'     " , "  `$CD = 'ColumnDefinition'  " , "  `$GC = 'Grid.Column'     " , 
+            " `$GCS = 'Grid.ColumnSpan'   " , "  `$GR = 'Grid.Row'          " , " `$GRS = 'Grid.RowSpan'    " , 
+            "  `$SI = 'SelectedIndex'     " , "  `$LA = 'Label'             " , "  `$BU = 'Button'          " , 
+            "  `$CO = 'Content'           " , "   `$Q = 'Name'              "
+            
+        $Glossary | % { IEX $_ } ; $GRD , $GCD = $RD , $CD | % { "$G.$_`s" } ; $Glossary += " `$GRD = '$GRD'" , " `$GCD = '$GCD'" 
+
+        $HAL  =  "Left" ,   "Center" ,  "Right" | % { "HorizontalAlignment = '$_'" } ; $HCAL =  $HAL | % { $_.Replace( 'lA' , 'lContentA' ) } 
+        $VAL  =   "Top" ,   "Center" , "Bottom" | % { "VerticalAlignment = '$_'"   } ; $VCAL =  $HAL | % { $_.Replace( 'lA' , 'lContentA' ) } 
+        
+        $SP = 0..13 | % { "    " * $_ }
+          
+        $PW , $PWB , $PWC = "Password" | % { "$_" , "$_`Box" , "$_`Char" }
+
+        # Generate XAML
+
+        $X   = @( 2 ; 0..5 | % { 13 } ; 10 , 9 ; 3..6 ; 6 , 5 ; 5..7 ; 7 , 6 , 6 ; 0..5 | % { 7 } ; 6 ; 0..11 | % { 6 } ; 5 ; 5..7 ; 7 , 6 , 6 ; 6..2 ) | % { $SP[$_] }  
+
+        $Y   = @( "<Window" , "  xmlns = '$Schema/presentation'" , "xmlns:x = '$Schema'" , "  Title = '$Author | Hybrid-DSC Module Installation'" , 
+                "  $W = '400'" , " $H = '450'" , "Topmost = 'True'" , "$( $HAL[1] )" , "  WindowStartupLocation = 'CenterScreen' >" , 
+                "<$GB $HD = 'Access Drive' $MA = '10' >" , "<$G>" , "<$GRD>" ; 4 , "" | % { "<$RD $H = '$_*' />" } ; "</$GRD>" , "<$G $GR = '0' >" , 
+                "<$GCD>" ; "" , 2.5 | % { "<$CD $W = '$_*' />" } ; "</$GCD>" , "<$GRD>" ; 0..5 | % { "<$RD $H = '*'/>" } ; "</$GRD>" ;
+
+               @( 0 ; "Name" | % { "Drive $_" , $_ } ) , @( 1 , "Network Path" , "Root" ) , @( 2 ; 0..1 | % { "Description" } ) ,
+               @( 3 ; "Username" | % { $_.Replace( 'n' , ' N' ) , $_ } ) , @( 4 ; @( "Password" ) * 2 ) , @( 5 ; @( "Confirm" ) * 2 ) | % {
+
+               If ( $_[0] -in 0..3 ) { $OBJ0 = $TB ; $OBJ1 = "" } If ( $_[0] -notin 0..3 ) { $OBJ0 = $PWB ; $OBJ1 = "$PWC = '*'" }
+            
+                "<$TBL $GR = '$( $_[0] )' $GC = '0' $MA = '10' $( $VAL[1] ) $( $HAL[2] ) >$( $_[1] )</$TBL>" , 
+                "<$OBJ0 $GR = '$( $_[0] )' $GC = '1' $MA = '10' $( $VAL[1] ) $OBJ1 $Q = '$( $_[2] )'></$OBJ0>" } ;
+                
+                "</$G>" , "<$G $GR = '1' >" , "<$GCD>" ; 0..1 | % { "<$CD $W = '*' />" } ; "</$GCD>" ; ( 0 , "Ok" ) , ( 1 , "Cancel" ) | % { 
+                "<$BU $GC = '$( $_[0] )' $Q = '$( $_[1] )' $CO = '$( $_[1] )' $MA = '15' />" } ; "</$G>" , "</$G>" , "</$GB>" , "</Window>" )
+
+        $XML = ( 0..51 | % { $X[$_] + $Y[$_] + "`n" } ) -join ''
+
+        # Load XAML
+
+        $NamedElements = "Name" , "Root" , "Description" , "Username" , "Password" , "Confirm" , "Ok" , "Cancel"
+
+        $GUI = Convert-XAMLtoWindow -Xaml $Xml -NE $NamedElements -PassThru
     
-    $GUI.Cancel.Add_Click({  $GUI.DialogResult = $False })
+        $GUI | % { 
+        
+            $_.Cancel.Add_Click({ $GUI.DialogResult = $False })
 
-    $GUI.Ok.Add_Click({
+            $_.Ok.Add_Click({
 
-        $0 = @( "Drive Label" , "Root Folder" , "UserName" , "Password" , "Confirmation" )
-        $1 = $0 | % { "$_ Error" } ; $2 = $0 | % { "Invalid $_" }
+            $MSG = "Drive Label" , "Root Folder" , "UserName" , "Password" , "Confirmation" | % { 
 
-    $MSG = 0..4 | % { "[ System.Windows.MessageBox ]::Show( '$( $1[$_] )' , '$( $2[$_] )' )" }
+                "[ System.Windows.MessageBox ]::Show( '$_ Error' , 'Invalid $_' )" }
 
-    If     ( $GUI.Name.Text -eq "" )                                   { IEX $MSG[0] }
-    ElseIf ( $GUI.Root.Text -eq "" )                                   { IEX $MSG[1] }
-    ElseIf ( $GUI.Username.Text -eq "" )                               { IEX $MSG[2] }
-    ElseIf ( $GUI.Password.Password -eq "" )                           { IEX $MSG[3] }
-    ElseIf ( $GUI.Confirm.Password  -eq "" )                           { IEX $MSG[4] }
-    ElseIf ( $GUI.Password.Password  -notmatch  $GUI.Confirm.Password) { IEX $MSG[4] }
+            If     ( $_.Name.Text -eq "" )                                  { IEX $MSG[0] }
+            ElseIf ( $_.Root.Text -eq "" )                                  { IEX $MSG[1] }
+            ElseIf ( $_.Username.Text -eq "" )                              { IEX $MSG[2] }
+            ElseIf ( $_.Password.Password -eq "" )                          { IEX $MSG[3] }
+            ElseIf ( $_.Confirm.Password  -eq "" )                          { IEX $MSG[4] }
+            ElseIf ( $_.Password.Password  -notmatch  $_.Confirm.Password ) { IEX $MSG[4] }
 
-    Else { $GUI.DialogResult = $True }
+            Else { $_.DialogResult = $True }
 
-    })
+            })
+    
+            $Null = $_.Name.Focus()
 
-        $Null = $GUI.Name.Focus()
+            $OP = Show-WPFWindow -GUI $_
 
-        $OP = Show-WPFWindow -GUI $GUI
+        }
 
         If ( $OP -eq $True )
         { 
@@ -160,7 +160,9 @@
         
         $Mod    = $ENV:PSModulePath.Split( ';' )
         $Mod    | % { If ( ( Test-Path $_ ) -ne $True ) { NI $_ -ItemType Directory } }
+
         $Hybrid = $Mod | % { GCI $_ -Recurse "*Hybrid-DSC.ps1*" } 
+
         $Hybrid | ? { $_ -ne $Null } | % { IPMO $_.FullName ; Echo "Module $( $_.FullName ) Loaded" }
 
         If ( $Hybrid -eq $Null )
@@ -173,29 +175,6 @@
             $Module = @( gci "$( $Drive.Root )\Modules" -Recurse "*Hybrid-DSC.ps*" | % { $_.FullName } )
             $Module | % { CP $_ $DSC.FullName }
             $Mod    | % { GCI $_ -Recurse "*Hybrid-DSC.PS1*" } | % { IPMO $_.FullName ; Echo "Module $( $_.FullName ) Loaded" }
-
-            $Report          = @{ Success = @( ) ; Failure = @( ) }
-            Start-PingSweep  | % { $Report.Success += @( $_.Success ) ; $Report.Failure += @( $_.Failure ) }
-            $New_IPAddress   = $Report.Failure | Select -First 1
-            $Adapter         = Get-NetAdapter               | ? { $_.Status       -eq   "Up" } | Select IFIndex
-            $Gateway         = $Adapter | Get-NetRoute      | ? { $_.RouteMetric  -eq     0  } | Select NextHop
-            $SubnetMask      = $Adapter | Get-NetIPAddress  | ? { $_.PrefixOrigin -eq "DHCP" } | Select PrefixLength
-
-            $IPAddress       = @{    InterfaceIndex = $Adapter.ifIndex
-                                          IPAddress = $New_IPAddress
-                                       PrefixLength = $SubnetMask.PrefixLength
-                                     DefaultGateway = $Gateway.NextHop
-                                      ValidLifeTime = [ TimeSpan ]::MaxValue
-                                  PreferredLifeTime = [ TimeSpan ]::MaxValue }
-
-            New-NetIpAddress @IPAddress
-    
-            $DNS             = @{    InterfaceIndex = $Adapter.ifIndex
-                                   ServerAddresses = "1.1.1.1" , "1.0.0.1" }
-
-            Set-DNSClientServerAddress @DNS
-    
-            0..10 | ? { ( Test-Connection -ComputerName "DSC$_" -Count 1 -EA 0 ) -eq $Null } | % { Rename-Computer "DSC$_" ; Restart-Computer }
         
         }
     }#                                                                            ____    ____    ____    ____    ____    ____    ____    ____    ____  
