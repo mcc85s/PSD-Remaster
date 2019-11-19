224fddacb8cfb9174f58ebcc93d9fab04273c1cf
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-11-19 17:03:59 -0500

    Update Hybrid-DSC.psm1

--- Modules/Hybrid-DSC/Hybrid-DSC.psm1
+++ Modules/Hybrid-DSC/Hybrid-DSC.psm1
@@ -27,30 +27,8 @@
         Get-XAML , Find-XAMLNamedElements , Get-NetworkInfo , Start-PingSweep , Get-NBTSCAN , Get-NetworkHosts , Get-TelemetryData ,
         Resolve-MacAddress , Start-NetworkInfo , Get-NetworkStatistics , Initialize-PortScan , Initialize-Server , Get-DSCFeatureList ,
         Get-DSCFeatureState , Show-Message , Invoke-Login , Export-Ini , Add-ACL , New-ACLObject, Import-DSCGraphics , Search-Registry , 
-        Resolve-DSCRoot , Install-DSCRoot
+        Resolve-UninstallList , Resolve-DSCRoot , Install-DSCRoot , Install-HybridDSC , Import-MDTModule
 
-        # Tells Michaelangelo that the Sistine Chapel sucks, and that you could paint that thing a lot better...
-
-        # Even if you can't. You just want to piss off Michaelangelo who spent 10 years against the damn ceiling being so obsessed with this ceiling
-        # painting, that "Hey, it doesn't matter. This painting will kick some ass. No really, it will. And...? I'm gonna paint this thing for the
-        # next 10 years if it's the last thing I do. No really. I will.
-
-        # And when he's done, you tell him... "Yeah, I could do that better in a lot less time."
-
-        # That is, until Michaelangelo goes to prison for immediately putting you in a coma for judging his work like that.
-        # No really, if you were alive then and you said that to him, he probably would get all butt-hurt about it.
-
-        # It's cool. Don't worry about it. The sistine chapel could be painted better by a bunch of 4 year olds. I mean it.
-
-        # I'm just kidding. Michaelangelo's Sistine Chapel ceiling painting doesn't suck. It's actually pretty cool. 
-        
-        # Just trying to vent, cause pretty pissed off. Not sure why. 
-        
-        # Could be all of the people getting paid a lot of money to censor me every god damn day.
-
-        # I know. I should probably apologize to Bill Gates for ripping off Gary Kildall's CPM program and then calling it DOS.
-
-        # But I won't. Because he did. 
                                                                                     #____ -- ____    ____ -- ____    ____ -- ____    ____ -- ____      
 <#___                                                                             __//¯¯\\__//==\\__/----\__//==\\__/----\__//==\\__/----\__//¯¯\\___  
 //¯¯\\___________________________________________________________________________/¯¯¯    ¯¯¯¯ ¯¯ ¯¯¯¯ ¯¯ ¯¯¯¯ ¯¯ ¯¯¯¯ ¯¯ ¯¯¯¯ ¯¯ ¯¯¯¯ ¯¯ ¯¯¯¯ ¯¯ ¯¯¯\\ 
