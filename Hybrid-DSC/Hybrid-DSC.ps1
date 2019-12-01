8ae12396c308c70dcf520180f605b6d36373510c
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-11-30 18:25:04 -0800

    Update Hybrid-DSC.ps1

--- Hybrid-DSC/Hybrid-DSC.ps1
+++ Hybrid-DSC/Hybrid-DSC.ps1
@@ -1,30 +1,69 @@
-
-    $Reg = ( gci *Hybrid-DSC.ps1* ).Directory.FullName
-
-    $Mod  = $ENV:PSModulePath.Split( ';' ) | ? { $_ -like "*$Env:UserName\Documents*" }
-        
-    $Mod , "$Mod\Hybrid-DSC" | % { 
-
-        If ( ! ( Test-Path $_ ) ) { NI $_ -ItemType Directory ; Write-Host "   Created [+] $_" -F 11 }
-        Else { Write-Host "  Detected [-] $_" -F 14 }
-        
+    Function Get-ScriptDirectory 
+    {
+        Return $( If ( $psise ) { Split-Path $psise.CurrentFile.FullPath } Else { $PSScriptRoot } )
     }
-    If ( $? ) { $Clean = GCI "$Mod\Hybrid-DSC" }
     
-    
-    "Hybrid-DSC" | % { 
-        
-        If ( Test-Path "$Reg\$_.zip" ) 
+    Function Install-HybridDSCModule
+    {
+        $Path  = $env:USERPROFILE
+        $Tree  = "Documents\WindowsPowerShell\Modules\Hybrid-DSC"
+        $Split = $Tree.Split( '\' )
+
+        ForEach ( $I in 0..( $Split.Count - 1 ) ) 
         {
-            Write-Host "Retrieving [~] $Reg\$_.zip" -F 14
+            "$Path\$( $Split[$I] )" | % {
 
-            $Splat = @{ Path            = "$Reg\$_.zip"
-                        DestinationPath = "$Mod\$_"     }
+                If ( ! ( Test-Path $_ ) )
+                {
+                    NI $_ -ItemType Directory | Out-Null
+                    Write-Host "   Created [+] $_" -F 11
+                }
 
-            Write-Host "Extracting [~] $Mod\$_" -F 14
+                Else 
+                {
+                    Write-Host "  Detected [~] $_" -F 14
+                }
 
-            Expand-Archive @Splat
+                $Path = $_
+            }
         }
 
+        $Destination = $Path
+
+        GCI $Destination | % { 
+        
+            If ( $_ -ne $Null ) 
+            { 
+                Write-Host " Detected [!] $( $_.Name ), removing" -F 11 
+                RI $_.FullName -Recurse -Force 
+            }
+        }
+
+        $Source = Get-ScriptDirectory
+    
+        "Hybrid-DSC" | % { 
+        
+            If ( Test-Path "$Source\$_.zip" ) 
+            {
+                Write-Host "Extracting [~] $Destination\$_" -F 14
+
+                Expand-Archive "$Source\$_.zip" "$Destination" -VB
+            }
+
+            Else
+            {
+                Write-Host " Exception [!] Zip file not found"
+            }
+        }
+
+        "Graphics" , "Control" , "Map" | % { 
+
+            Write-Host "Extracting [~] $Destination\$_.zip" -F 11
+
+            Expand-Archive "$Destination\$_.zip" $Destination
+        }
+    
         IPMO Hybrid-DSC -Force
     }
+
+    Install-HybridDSCModule
