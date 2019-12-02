dc6cb11ccd3cdcef2c37203a08ecab3270cffc0a
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-12-02 10:18:03 -0500

    Add files via upload

---
+++ Hybrid-DSC/Functions/New-SubTable.ps1
@@ -0,0 +1,26 @@
+﻿    Function New-SubTable # Converts array of keys and values into an Object ___________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
+    {#/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯      
+        [ CmdLetBinding ( ) ] Param (
+
+            [ Parameter ( Position = 0 , Mandatory = $True , ValueFromPipeline = $True ) ][ String [] ]  $Items ,
+            [ Parameter ( Position = 1 , Mandatory = $True , ValueFromPipeline = $True ) ][ String [] ] $Values )
+
+        $Table = [ PSCustomObject ]@{ }
+
+        If ( $Items.Count -eq $Values.Count )
+        {
+            If ( $Items.Count -gt 1 ) 
+            { 
+                $Count = 0..( $Items.Count - 1 ) 
+            }
+            
+            $Count | % { $Table | Add-Member -MemberType NoteProperty -Name "Item:$_" -Value @{ ID = "$( $Items[$_] )" ; Value = "$( $Values[$_] )" } }
+        }
+        
+        If ( $Items.Count -ne $Values.Count ) 
+        { 
+            Throw '$Items -ne $Values' 
+        }
+        
+        Return $Table                                                                #____ -- ____    ____ -- ____    ____ -- ____    ____ -- ____      
+}
