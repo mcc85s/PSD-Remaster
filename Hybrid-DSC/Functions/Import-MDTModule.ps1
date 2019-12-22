377876f10f7fef80c51d591367d6456fc8b434f2
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-12-21 20:54:11 -0500

    Delete Import-MDTModule.ps1

--- Hybrid-DSC/Functions/Import-MDTModule.ps1
+++
@@ -1,7 +0,0 @@
-﻿    Function Import-MDTModule # Loads the module for MDT _______________________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
-    {#/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯      
-       
-        GP "HKLM:\Software\Microsoft\Deployment 4" | % { GCI $_.Install_Dir "*Toolkit.psd1" -Recurse } | % { IPMO $_.FullName -VB }
-        Write-Theme -Action "Module [+]" "Microsoft Deployment Toolkit"
-                                                                                    #____ -- ____    ____ -- ____    ____ -- ____    ____ -- ____      
-}
