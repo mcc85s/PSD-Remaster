8915abfabefa2910dca08e6db1f3e33df229686c
Michael C Cook Sr <44849131+mcc85s@users.noreply.github.com>
2019-12-21 20:52:26 -0500

    Delete Add-ACL.ps1

--- Hybrid-DSC/Functions/Add-ACL.ps1
+++
@@ -1,7 +0,0 @@
-﻿    Function Add-ACL # Changes permissions of application pool/VHost ____________________//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯\\__//¯¯¯  
-    {#/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯ -- ¯¯¯¯    ¯¯¯¯      
-       [ CmdletBinding () ] Param ( [ String ] $Path , [ FileSystemAccessRule ] $ACL ) 
-
-            Get-ACL -Path $Path | % { $_.AddAccessRule( $ACL ) ; Set-ACL -Path $Path -ACLObject $_ }
-                                                                                    #____ -- ____    ____ -- ____    ____ -- ____    ____ -- ____      
-}
